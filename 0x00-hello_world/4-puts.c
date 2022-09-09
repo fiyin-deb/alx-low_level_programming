0816068a1ce416dd880fc4e3f60d0120f6d76d87
Fiyinfoluwa <fiyinfoluwatolu6@gmail.com>
2022-09-09 12:17:07 -0700

    4-puts.c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,28 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                                                      
 
+                                                                                                                                        
 
+/**                                                                                                                                     
 
-/**
- *
- *  * main - print the string in the put function
- *
- *  Description: using the main function
- *  this program prints "Programming is like building a multilingual puzzle
- *  Return: 0
- */
-int main(void)
-{
-        puts("\"programming is like building a multilingual puzzle");
-	return (0)
-}
+ *                                                                                                                                      
+
+ *  * main - print the string in the put function                                                                                       
+
+ *                                                                                                                                      
+
+ *  Description: using the main function                                                                                                
+
+ *  this program prints "Programming is like building a multilingual puzzle                                                             
+
+ *  Return: 0                                                                                                                           
+
+ */                                                                                                                                     
+
+int main(void)                                                                                                                          
+
+{                                                                                                                                       
+
+        puts("\"programming is like building a multilingual puzzle");                                                                   
+
+        return (0)                                                                                                                      
+
