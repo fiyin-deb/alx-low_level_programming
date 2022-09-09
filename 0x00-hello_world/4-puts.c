1ecb14724eb6172fb374b133395dc3abe0095d47
Fiyinfoluwa <fiyinfoluwatolu6@gmail.com>
2022-09-09 12:31:52 -0700

    4-puts.c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,19 +2,11 @@
 
                                                                                                                                         
 
-/**                                                                                                                                     
+/**                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 
- *                                                                                                                                      
+ *  Description: main - Prints out a string to stdout.                                                                                                                                                         
 
- *  * main - print the string in the put function                                                                                       
-
- *                                                                                                                                      
-
- *  Description: using the main function                                                                                                
-
- *  this program prints "Programming is like building a multilingual puzzle                                                             
-
- *  Return: 0                                                                                                                           
+ *  Return: 0 if success.                                                                                                                           
 
  */                                                                                                                                     
 
@@ -24,5 +16,5 @@ int main(void)
 
         puts("\"programming is like building a multilingual puzzle");                                                                   
 
-        return (0)                                                                                                                      
+        return (0) ;                                                                                                                     
 
