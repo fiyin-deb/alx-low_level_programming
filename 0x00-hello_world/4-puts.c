646d81c8e9f6a68d6ee151eabd0f2d9834ffd55d
Fiyinfoluwa <fiyinfoluwatolu6@gmail.com>
2022-09-10 11:19:59 -0700

    4-putss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,20 +3,21 @@
 
 
 /**
-
- * main - Entry point
-
  *
-
- * Return: Always 0 (Success)
-
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
 {
 
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-return(0);
+	return(0);
 
+}                     
