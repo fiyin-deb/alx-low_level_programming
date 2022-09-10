bb4e54e8e969b209088bd040d402a6c2a557d465
Fiyinfoluwa <fiyinfoluwatolu6@gmail.com>
2022-09-10 12:49:48 -0700

    4-putt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
  *
- * Description: main - Prints oout a string to stdout
+ * Description:main-Prints oout a string to stdout
  * Return: Always 0 (Success)
  *
  *    
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts
+		("\"Programming is like building a multilingual puzzle");
 	return(0);
 }                     
