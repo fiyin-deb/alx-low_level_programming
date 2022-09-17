ac44e17cb7be95e51061126cd76f8ee94bb43ceb
Fiyinfoluwa <fiyinfoluwatolu6@gmail.com>
2022-09-17 11:30:16 -0700

    24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,20 +13,20 @@ void jack_bauer(void)
 
 	while (i < 24)
 	{
-		j = 0;
+	j = 0;
 
 	while (j < 60)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
-			j++;
+		_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
+		_putchar(':');
+		_putchar((j / 10) + '0');
+		_putchar((j % 10) + '0');
+		_putchar('\n');
+		j++;
 
 		}
-		i++;
+                i++;
 
 	}
 }
