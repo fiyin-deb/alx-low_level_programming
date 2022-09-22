7595844473a2459c63c1b8ddb79340eb2f0e669f
Fiyinfoluwa <fiyinfoluwatolu6@gmail.com>
2022-09-22 14:13:23 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,9 +4,10 @@
   * print_number - print numbers chars
   * @n: integer params
   * Return: 0
-*/
+ **/
 
 void print_number(int n)
+
 {
 	unsigned int n1;
 
@@ -16,13 +17,16 @@ void print_number(int n)
 
 	{
 		_putchar('-');
+
 		n1 = -n;
 	}
 
 	if (n1 / 10 != 0)
+
 	{
 		print_number(n1 / 10);
 	}
 
 	_putchar((n1 % 10) + '0');
+
 }
